9b990792677a3cb28c2ab40ee831b3d773ac7c7d
LuckDev <lukedru@yahoo.com>
2022-12-04 12:01:33 -0800

    Initial Commit

---
+++ Assistant GPT/Assistant_GPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  Assistant_GPTApp.swift
+//  Assistant GPT
+//
+//  Created by Luke Drushell on 12/4/22.
+//
+
+import SwiftUI
+
+@main
+struct Assistant_GPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
